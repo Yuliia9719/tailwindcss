16d91c0a564a320b7bbe9d8fe39476042d09a26d
Yuliia <iyulia1997len@gmail.com>
2023-12-12 23:26:37 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        main: ['Lora', 'serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
